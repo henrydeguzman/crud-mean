d906f9ef0d0362399438f70b0827d677be8a06ee
henry <henrydeguzman.java73@gmail.com>
2019-09-16 17:33:16 +0800

    creating links created post the post the list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'crud-mean';
+    storedPost = [];
+
+    onPostAdded(p) {
+        this.storedPost.push(p);
+    }
 }
