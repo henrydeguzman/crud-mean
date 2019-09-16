1d49af6739e957ecde8af09e2c9a5e32f9cc586e
henry <henrydeguzman.java73@gmail.com>
2019-09-16 17:51:22 +0800

    Implement model and html forms data.

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,17 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
     styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-    enteredContent = '';
-    enteredTitle = '';
-
-    @Output() postCreated = new EventEmitter();
-    onAddPost() {
-        const post = {title: this.enteredTitle, content: this.enteredContent};
+    @Output() postCreated = new EventEmitter<Post>();
+    onAddPost(a: NgForm) {
+        if (a.invalid) { return; }
+        const post: Post = {title: a.value.title, content: a.value.content};
         this.postCreated.emit(post);
     }
 }
