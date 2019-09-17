f253e1e32e3b52eb519d2738c602f24651455572
henry <henrydeguzman.java73@gmail.com>
2019-09-17 08:25:55 +0800

    adding nodemon and configuring server.js

--- server.js
+++ server.js
@@ -1,7 +1,49 @@
+const app = require('./backend/app');
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.end('This is my first response');
-});
+const normalizePort = val => {
+    var port = parseInt(val, 10);
+    if (isNaN(port)) {
+        // name pipe
+        return val;
+    }
 
-server.listen(process.env.PORT | 3000);
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+    return false;
+}
+
+const onError = error => {
+    if (error.syscall !== "listen")  {
+        throw error;
+    }
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+    switch (error.code) {
+        case "EACCESS":
+            console.error(bind + " requires elevated privileges");
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use");
+            break;
+        default:
+            throw error;
+    }
+}
+
+const onListening = () => {
+    const addr = server.address();
+    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+    debug("Listening on " + bind);
+}
+
+
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set('port', port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listeing", onListening)
+server.listen(port);
