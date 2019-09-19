f1a44068d88063e1bea768f1d5c63f603ff0f5b9
henry <henrydeguzman.java73@gmail.com>
2019-09-19 22:41:48 +0800

    Add user signin and handler inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import {
@@ -19,18 +19,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         PostCreateComponent,
         HeaderComponent,
-        PostListComponent
+        PostListComponent,
+        LoginComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        ReactiveFormsModule,
+        ReactiveFormsModule, FormsModule,
         BrowserAnimationsModule,
         MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule, MatProgressSpinnerModule, MatPaginatorModule,
         HttpClientModule
